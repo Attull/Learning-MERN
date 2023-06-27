af8192a22b5a60f796866dbfc4f4ba2fa1ae6446
Attull <atul852798@gmail.com>
2023-06-27 12:34:10 +0530

    integrat API in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [data, setData] =  useState([])
+
+  const fetchData = () =>{
+    fetch("http://localhost:5000/students")
+      .then(res=> res.json())
+      .then(data =>{
+          console.log(data)
+          setData(data)
+      })
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[])
+
   return (
     <div className="App">
-
+        {
+          data.map(val =>{
+            return <h1>{val.name}</h1>
+          })
+        }
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import Express from "express"
+import cors from 'cors'
 
 const app =  Express()
 
+app.use(cors())
 
 app.get('/', (req, res)=>{
     res.send("Heyyyy Nodemon")
